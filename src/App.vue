3e379a9844dc6c61e980a45f8bb314204e391f4a
kaanberke34 <berkeyilmaz34@gmail.com>
2022-06-10 20:01:26 +0300

    Footer added

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,16 @@
     <b-container>
       <router-view />
     </b-container>
+    <Footer />
   </div>
 </template>
 <script>
 import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 export default {
   components: {
     Navbar,
+    Footer,
   },
 };
 </script>
