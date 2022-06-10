90a6003a79b956cd16c6d7ab1967a404d82db953
kaanberke34 <berkeyilmaz34@gmail.com>
2022-06-10 19:51:41 +0300

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-container>
+        <b-navbar-brand href="/">
+          <img
+            src="https://placekitten.com/g/30/30"
+            class="d-inline-block align-top"
+            alt="Kitten"
+          />Mutabik</b-navbar-brand
+        >
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="/menu">Menu</b-nav-item>
+            <b-nav-item href="/location">Location</b-nav-item>
+            <b-nav-item href="/about">About Us</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </nav>
+</template>
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+<style scoped>
+</style>
---
+++ src/views/LocationView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="location">
+    <h1>This is a location page</h1>
+  </div>
+</template>
